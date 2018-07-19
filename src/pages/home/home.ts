1e49e20df5641f33d7cc78ac8c15550ef8c2b596
AllenHiggins <allenghiggins@gmail.com>
2018-07-19 12:02:00 +0100

    testing a apikey class example only

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {API} from '../../app/api'
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,14 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  private n = new API();
+  key: string;
 
+
+
+  constructor(public navCtrl: NavController) {
+    this.key = this.n.getKey();
+    console.log(this.key);
   }
 
 }
